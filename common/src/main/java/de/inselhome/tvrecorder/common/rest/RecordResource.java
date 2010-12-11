dcf77b9c0f3a0a85d4ac54d99fb6fc168e45a51c
Ingo Weinzierl <ingo_weinzierl@web.de>
2010-12-11 10:37:24 +0100

    Initial checkin of the common package, used for the communication between server and clients.

---
+++ common/src/main/java/de/inselhome/tvrecorder/common/rest/RecordResource.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (C) 2010 Ingo Weinzierl (ingo_weinzierl@web.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package de.inselhome.tvrecorder.common.rest;
+
+import org.restlet.resource.Post;
+
+import de.inselhome.tvrecorder.common.objects.Job;
+
+
+/**
+ * This class represents the interface description of the job resource. This
+ * interface defines a basic methods to add new jobs.
+ *
+ * @author <a href="mailto: ingo_weinzierl@web.de">Ingo Weinzierl</a>
+ */
+public interface RecordResource {
+
+    @Post
+    public void add(Job job);
+}
+// vim:set ts=4 sw=4 si et sta sts=4 fenc=utf8 :
