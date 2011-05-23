b4d8a9776e545ec09d645e7927e742926df42c47
Ingo Weinzierl <ingo.weinzierl@intevation.de>
2011-05-23 18:18:55 +0200

    Added master and workers to fetch tv show information and update a predefined set of channels via RSS feeds.

---
+++ server/src/main/java/de/inselhome/tvrecorder/server/tvguide/TvShowUpdateEvent.java
@@ -0,0 +1,42 @@
+/**
+ * Copyright (C) 2011 Ingo Weinzierl (ingo_weinzierl@web.de)
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License as published by the Free Software
+ * Foundation; either version 3 of the License, or (at your option) any later
+ * version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU General Public License along with
+ * this program; if not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+package de.inselhome.tvrecorder.server.tvguide;
+
+import java.util.List;
+
+import de.inselhome.tvrecorder.common.objects.ChannelWithTvGuide;
+
+
+/**
+ * @author <a href="mailto: ingo_weinzierl@web.de">Ingo Weinzierl</a>
+ */
+public class TvShowUpdateEvent {
+
+    protected List<ChannelWithTvGuide> updated;
+
+
+    public TvShowUpdateEvent(List<ChannelWithTvGuide> updated) {
+        this.updated = updated;
+    }
+
+
+    public List<ChannelWithTvGuide> getUpdated() {
+        return updated;
+    }
+}
+// vim:set ts=4 sw=4 si et sta sts=4 fenc=utf8 :
