63e69d391ea594851c4e8b546d141df8074b4ef7
Ingo Weinzierl <ingo_weinzierl@web.de>
2011-09-28 21:37:40 +0000

    Added a tool epgdownload.py to contrib directory which allows reading EPG data from OTR and writing those data to XmlTv file.

---
+++ server/contrib/epgloader/model.py
@@ -0,0 +1,29 @@
+#
+# Copyright (C) 2011 by Ingo Weinzierl
+# Authors: Ingo Weinzierl <ingo_weinzierl@web.de>
+#
+# This program is free software; you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation; either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more
+# details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program; if not, see <http://www.gnu.org/licenses/>.
+
+class Program:
+
+    def __init__(self, title, description, start, end):
+        self.title       = title
+        self.description = description
+        self.start       = start
+        self.end         = end
+
+    def __str__(self):
+        return "|%s,%s,%s,%s|" % (
+               self.title, self.description, self.start, self.end)
+
